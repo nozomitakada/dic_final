d9a557527faeaf060bbfa45b15fcc799787d892b
nozomi takada <g031n096@s.iwate-pu.ac.jp>
2018-12-28 08:44:57 +0000

    init

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,7 +1,7 @@
 class Owner < ApplicationRecord
     has_many :houses
     validates :name, presence: true,length: { maximum: 255 }
-     validates :phone_number, presence: true,length: { maximum: 255 }
+    # validates :phone_number, presence: true,length: { maximum: 255 }
     validates :email, presence: true,length: { maximum: 255 },
             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
             before_validation { email.downcase! }
